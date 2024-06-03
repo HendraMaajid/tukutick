b33b46f9bc1f217673dda6d93440843923eae57c
Ageng <agengpraba@gmail.com>
2024-06-03 23:57:17 +0700

    Hak akses
    Update sidebar untuk hak akses setiap role

--- resources/views/layouts/app/navbar.blade.php
+++ resources/views/layouts/app/navbar.blade.php
@@ -199,7 +199,7 @@
        <li class="dropdown"><a href="#" data-toggle="dropdown"
            class="nav-link dropdown-toggle nav-link-lg nav-link-user">
            <img alt="image" src="../../assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-           <div class="d-sm-none d-lg-inline-block">Hi, Administrator</div>
+           <div class="d-sm-none d-lg-inline-block">{{Auth::user()->username;}}</div>
          </a>
          <div class="dropdown-menu dropdown-menu-right">
            <div class="dropdown-title">Logged in 5 min ago</div>
@@ -213,15 +213,15 @@
              <i class="fas fa-cog"></i> Settings
            </a>
            <div class="dropdown-divider"></div>
-              <a href="{{ route('logout') }}" class="dropdown-item has-icon text-danger"
-                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                <i class="fas fa-sign-out-alt"></i> Logout
-              </a>
+           <a href="{{ route('logout') }}" class="dropdown-item has-icon text-danger"
+             onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+             <i class="fas fa-sign-out-alt"></i> Logout
+           </a>
 
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-            </form>
-            
+           <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+             @csrf
+           </form>
+
 
          </div>
        </li>
--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -11,6 +11,7 @@
               <a href="{{url('admin')}}"><i class="fas fa-fire"></i><span>Dashboard</span></a>
             </li>
             <li class="menu-header">Master</li>
+            @if(Auth::user()->role == 'admin')
             <li>
               <a href="{{url('akun')}}"><i class="fas fa-user"></i><span>Users</span></a>
             </li>
@@ -18,13 +19,14 @@
               <a href="{{url('kategori')}}"><i class="fas fa-list"></i><span>Kategori</span></a>
             </li>
             <li>
-              <a href="{{url('event')}}"><i class="fas fa-calendar-alt"></i><span>Event</span></a>
-            </li>
-            <li>
               <a href="{{url('penyelenggara')}}"><i class="fas fa-user-tie"></i><span>Penyelenggara</span></a>
             </li>
             <li>
               <a href="{{url('customer')}}"><i class="fas fa-users"></i><span>Customer</span></a>
+            </li>
+            @elseif(Auth::user()->role == 'penyelenggara')
+            <li>
+              <a href="{{url('event')}}"><i class="fas fa-calendar-alt"></i><span>Event</span></a>
             </li>
             <li>
               <a href="{{url('transaksi')}}"><i class="fas fa-dollar-sign"></i><span>Transaksi</span></a>
@@ -35,6 +37,10 @@
             <li>
               <a href="{{url('pemenang')}}"><i class="fas fa-ticket-alt"></i><span>Pemenang</span></a>
             </li>
+            <li>
+              <a href="{{url('tiket')}}"><i class="fas fa-ticket-alt"></i><span>Tiket</span></a>
+            </li>
+            @endif
           </ul>
           <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
         </aside>
